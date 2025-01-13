ddf652944c77dc1d33eb2de7659420ec1d053d71
Ichiden <denverbigayan1@gmail.com>
2025-01-14 00:09:50 +0800

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import polyfillNode from "rollup-plugin-polyfill-node";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +13,27 @@ export default defineConfig({
       define: {
         global: "globalThis", // Polyfill for global
       },
-      plugins: [NodeModulesPolyfillPlugin()],
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
     },
   },
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      stream: "stream-browserify",
+      buffer: "buffer",
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [polyfillNode()],
+    },
   },
 });
