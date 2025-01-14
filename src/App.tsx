2080043b5abff31e504ec88d996c65b5b574c4ea
Ichiden <denverbigayan1@gmail.com>
2025-01-14 15:13:02 +0800

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "./App.css";
-import Peer from "simple-peer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
   useEffect(() => {
-    console.log(new Peer({}));
+    navigator?.mediaDevices
+      ?.getUserMedia({ video: true, audio: true })
+      .then((stream: MediaStream) => {
+        if (videoRef.current) videoRef.current.srcObject = stream;
+      });
   }, []);
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <div>
+      <video ref={videoRef} autoPlay></video>
     </div>
   );
 }
